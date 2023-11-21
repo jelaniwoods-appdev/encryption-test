91f37ca834b52bb21c2e92938d99a33a3f7cdb50
Jelani Woods <jelani@firstdraft.com>
2023-11-21 09:51:05 -0600

    Create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+["alice", "bob", "carol"].each do |name|
+  user = User.find_or_create_by(email: "#{name}@example.com", name: name)
+  user.update password: "password"
+end
+
+Post.destroy_all
+User.all.each do |user|
+  rand(3..8).times do |i|
+    desc = (0...50).map { ('a'..'z').to_a[rand(26)] }.join
+    user.posts.create(title: "user.name#{i}", description: desc)
+  end
+end
